0fe049a4c626bb6823d77502a6c170b0c98cc12f
Victoria Querequeta <vickyquerequeta@gmail.com>
2023-05-03 10:57:20 -0300

    Second commit

---
+++ listas.py
@@ -0,0 +1,13 @@
+# Uso de listas en Python
+# con list
+milista=list("Hola")
+print(milista)
+print(milista[2])
+
+# sin list
+otralista=["Hola", "Juan", 15]
+print(otralista)
+print(otralista[2])
+
+# Lista dentro de otra lista
+milista=list(["Juan Pérez", 45568417, 6,7,8,7], ["María López", 42491573, 8,7,8,9])
